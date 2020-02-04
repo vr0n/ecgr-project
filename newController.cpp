3b88384ee802e7ade13734609e2ed6231ad4be65
rtomas2 <rtomas2@uncc.edu>
2020-02-04 10:50:55 -0500

    float class, bin to dec, no special cases

---
+++ newController.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+class MyFloat{
+	public:
+	int sign = 0;
+	int floatArray[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	int exp = 0;
+	int mant = 0;
+	MyFloat(string rawBin){
+		//TODO: insert checks for special cases
+		int tempExponent = 0;
+
+		for(int i = 31; i>=0; i--){
+			floatArray[i] = ((int)rawBin[i]) - 48;
+		}
+
+		sign = floatArray[0];
+
+		for(int i = 1; i<9; i++){
+			if(floatArray[i] == 1){
+				exp = exp + pow(2, (9-i));
+			}
+		}
+		for(int i = 9; i<32; i++){
+			if(floatArray[i] == 1){
+				mant = mant + pow(2, (32-i));
+			}
+		}
+	}
+};
+
+double binToDec(MyFloat tempFloat){
+	double dec = 0.0;
+	dec = pow(tempFloat.mant, tempFloat.exp);
+	if(tempFloat.sign){
+		dec = -dec;
+	}
+	return dec;
+};
+
+int main(){
+	string testFloat = "11001000001111001111000001011010";
+	MyFloat myTestFloat(testFloat);
+	
+	double val = binToDec(myTestFloat);
+	cout << val << endl;
+	return 0;
+}
